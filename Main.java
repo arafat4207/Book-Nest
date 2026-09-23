fdcb8c4d145d0e609c77da7d66f2eb0ea647b767
abdullah522567 <chowdhuryabdullah5225@gmail.com>
2026-09-23 20:15:24 +0600

    Add Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main (String []args ){
-        Scanner sc = new Scanner(System.in);
-    System.out.println("Enter your name: ");
-    String name = sc.nextLine();
-    System.out.println("Enter your id: ");
-    int id = sc.nextInt();
-    }
+       Book book1 = new Book("The Great Gatsby", 1);
+       book1.displayInfo();
+       
+ }
 }
