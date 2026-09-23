a132162eab3d155fb7896bf7cfb2d8325a186e5a
Arafat Shikder Samim <arafatshikdersazim@gmail.com>
2026-09-24 00:16:16 +0600

    updated user java and also reader.java for login system

--- User.java
+++ User.java
@@ -1,3 +1,30 @@
-public class User {
+import java.io.Serializable;
+
+public abstract class User implements Serializable {
+
+    private String  fullname ;
+    private String username ;
+    private String  password ;
+
+    public User ( String fullname , String username , String password ){
+        this.fullname = fullname ;
+        this.username = username ;
+        this. password = password ;
+    }
+
+    public String getfullname(){
+        return fullname ;
+    }
+    public String  getuserName(){
+        return username ;
+    }
+
+    public String getPassword()
+    {
+        return password ;
+    }
+
+    public abstract String getRole();
+
 
 }    
