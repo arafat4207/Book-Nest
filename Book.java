51bea12f94476bdc3935c4ea70e36e9720540ba8
Arafat Shikder Samim <arafatshikdersazim@gmail.com>
2026-09-23 22:54:14 +0600

    changed by arafat

--- Book.java
+++ Book.java
@@ -1,13 +1,4 @@
 public class Book{
-    String name;
-    int id;
-    Book(String name, int id){
-        this.name = name;
-        this.id = id;
-    }
-    public void displayInfo(){
-        System.out.println("Book Name: " + name);
-        System.out.println("Book ID: " + id);
-    }
+   
 
 }
